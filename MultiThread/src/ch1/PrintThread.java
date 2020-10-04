e099640b34b1d8bc36b4afae9e52d8b3356ecf23
jy <jy@10.0.0.90>
2020-10-03 21:17:48 -0400

    ch1 sample

---
+++ MultiThread/src/ch1/PrintThread.java
@@ -0,0 +1,13 @@
+package ch1;
+
+public class PrintThread extends Thread {
+	private String message;
+	public PrintThread(String message) {
+		this.message = message;
+	}
+	public void run() {
+		for (int i=0; i<10000; i++) {
+			System.out.println(message);
+		}
+	}
+}
